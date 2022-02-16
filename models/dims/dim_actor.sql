fcaa08edd470373d22a7dd6535b41334158576e5
Karthik Maddipoti <mkarthik130295@gmail.com>
2022-02-16 02:27:22 +0000

    create basic model in class

---
+++ models/dims/dim_actor.sql
@@ -0,0 +1,8 @@
+{{ config(materialized='table') }}
+
+select 
+    actor_id as actor_key,
+    CONCAT_WS(' ', last_name,  first_name) as full_name
+from 
+    {{ref('stage_actor')}}
+  --  "SAKILA_DW"."DBT_KMADDIPOTI"."STAGE_ACTOR"
